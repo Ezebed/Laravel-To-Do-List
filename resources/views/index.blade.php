30051ca1ac223a12153476b306d769f44f7f8e7c
Ezebed <obedvillegas.02@gmail.com>
2024-02-28 15:03:20 -0400

    livewire install and todocontroller

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.MainLayout')
+
+@section('title','To Do List')
+
+@section('content')
+    <div class='w-ful min-h-full '>
+        hola
+    </div>
+@endsection
---
+++ resources/views/layouts/MainLayout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    @livewireStyles
+    @vite('resources/css/app.css')
+</head>
+<body class="antialiased bg-gray-700 text-white">
+    <header>
+        @include('layouts.navigation')
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    @livewireScripts
+</body>
+</html>
