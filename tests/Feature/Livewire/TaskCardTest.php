aae1839e1fad51aa6d31041a12c2edd61e5e7800
Ezebed <obedvillegas.02@gmail.com>
2024-03-18 18:17:44 -0400

    add taskCard tests

--- tests/Feature/Livewire/TaskCardTest.php
+++ tests/Feature/Livewire/TaskCardTest.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
 use Tests\TestCase;
 
+use App\Models\User;
+use App\Models\Task;
+
 class TaskCardTest extends TestCase
 {
     /** @test */
@@ -16,4 +19,37 @@ class TaskCardTest extends TestCase
         Livewire::test(TaskCard::class)
             ->assertStatus(200);
     }
+
+    /** @test */
+    public function see_on_page()
+    {
+        $user = User::factory()->create();
+
+        Task::factory()->create(['user_id' => $user->id]);
+
+        Livewire::actingAs($user);
+
+        $this->get(route('index'))
+            ->assertSeeLivewire(TaskCard::class);
+    }
+
+    /** @test */
+    public function properties_set()
+    {
+        $user = User::factory()->create();
+
+        $task = Task::factory()->create([
+            'user_id' => $user->id,
+            'body' => 'text of task',
+            'completed' => false,
+        ]);
+
+        Livewire::actingAs($user)
+            ->test(TaskCard::class)
+            ->set('taskID',$task->id)
+            ->set('taskBody',$task->body)
+            ->set('taskStatus',$task->completed)
+            ->assertSee($task->body);
+    }
+     
 }
